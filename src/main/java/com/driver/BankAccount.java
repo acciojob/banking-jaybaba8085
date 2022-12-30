21fec5bd1d5c67eede96d3d4ae65ec0fd5c8742b
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-30 21:01:02 +0530

    HUHU

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -40,21 +40,34 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if (sum > 9 * digits) {
+//        if (sum > 9 * digits) {
+//            throw new Exception("Account Number can not be generated");
+//        }
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < digits; i++) {
+//            if (sum >= 9) {
+//                sb.append('9');
+//                sum -= 9;
+//            } else {
+//                sb.append((char)('0' + sum));
+//                sum = 0;
+//            }
+//        }
+//        return sb.toString();
+        if(digits*9 < sum) {
             throw new Exception("Account Number can not be generated");
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < digits; i++) {
-            if (sum >= 9) {
-                sb.append('9');
-                sum -= 9;
-            } else {
-                sb.append((char)('0' + sum));
-                sum = 0;
-            }
+        String accountNumber = "";
+        while(sum > 9){
+            accountNumber += '9';
+            sum -= 9;
         }
-        return sb.toString();
+        accountNumber += (sum + "");
+        while(accountNumber.length() < digits){
+            accountNumber += '0';
+        }
+        return accountNumber;
     }
 
     public void deposit(double amount) {
