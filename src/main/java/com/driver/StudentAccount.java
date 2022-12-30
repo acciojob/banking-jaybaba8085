b88563659d7f0bf6ed5b054b2423c832ccfd95e6
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2022-12-30 16:54:32 +0530

    Check

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -12,4 +12,29 @@ public class StudentAccount extends BankAccount{
       this.institutionName=institutionName;
     }
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getGetInstitutionName() {
+        return getInstitutionName;
+    }
+
+    public void setGetInstitutionName(String getInstitutionName) {
+        this.getInstitutionName = getInstitutionName;
+    }
 }
